b7bab366ed05a0d12e2fd6bf6d4cd5f8dc31b668
Daniel Adelantado <dadelantado@gmail.com>
2015-10-31 15:32:49 -0600

    Change all to use classes

--- playlist.py
+++ playlist.py
@@ -1,40 +1,47 @@
+#!/usr/bin/python
 import urllib, json, subprocess, time, sys, getopt
 
-print "Playlist id?",
-playlist = raw_input()
+def getplaylist(playlistid):
+    apikey = 'AIzaSyA7s-mBPBU5snEKPZ7CAuLwIuvGa6hRGyc'
+    inp = urllib.urlopen(r'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId={0}&key={1}&maxResults=50'.format(playlistid, apikey))
+    resp = json.load(inp)
+    inp.close()
+    playlist = resp['items']
+    return playlist
 
-apikey = 'AIzaSyA7s-mBPBU5snEKPZ7CAuLwIuvGa6hRGyc'
+def generatefile(playlistid):
+    print 'Generating playlist ...'
+    playlist = getplaylist(playlistid)
+    ind = 0
+    firstvideo = playlist[ind]['snippet']['resourceId']['videoId']
+    file = open('playlist.html', 'w')
+    file.write('<div class="responsive-video-list"><div class="featured-video"><iframe width="100%" height="100%" src="https://www.youtube.com/embed/'+firstvideo+'?autoplay=0&amp;rel=0&amp;showinfo=0&amp;modestbranding=1&amp;autohide=1" frameborder="0" allowfullscreen id="FeaturedVideoID"></iframe></div><ul>')
 
-#1st Part: retreive uploads id, forUsername(with username) or id (channel id)
+    #Add videos from YouTube playlist to HTML playlist
 
-inp = urllib.urlopen(r'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId={0}&key={1}&maxResults=50'.format( playlist, apikey ) )
-resp = json.load(inp)
-inp.close()
+    for videos in playlist:
+        videoid = playlist[ind]['snippet']['resourceId']['videoId']
+        title = playlist[ind]['snippet']['title'].encode('ascii', 'replace')
+    	file.write('<li><a onclick="switchVideo(\'www.youtube.com/embed/'+videoid+'?autoplay=1&amp;rel=0&amp;showinfo=0&amp;modestbranding=1&amp;autohide=1\');" href="javascript:void(0);"> <img src="http://img.youtube.com/vi/'+videoid+'/0.jpg">'+title+'</a></li>')
+    	ind += 1
 
-result = resp['items']
+    file.write('</ul></div>')#Close dive
 
-
-
-
-ind = 0
-#Add the featured video to the playlist
-print 'Generatin playlist ...'
-firstvideo = result[ind]['snippet']['resourceId']['videoId']
-file = open('playlist.html', 'w')
-file.write('<div class="responsive-video-list"><div class="featured-video"><iframe width="100%" height="100%" src="https://www.youtube.com/embed/'+firstvideo+'?autoplay=0&amp;rel=0&amp;showinfo=0&amp;modestbranding=1&amp;autohide=1" frameborder="0" allowfullscreen id="FeaturedVideoID"></iframe></div><ul>')
-
-#Add videos from YouTube playlist to HTML playlist
-for videos in result:
-	videoid = result[ind]['snippet']['resourceId']['videoId']
-	title = result[ind]['snippet']['title'].encode('ascii', 'replace')
-	file.write('<li><a onclick="switchVideo(\'www.youtube.com/embed/'+videoid+'?autoplay=1&amp;rel=0&amp;showinfo=0&amp;modestbranding=1&amp;autohide=1\');" href="javascript:void(0);"> <img src="http://img.youtube.com/vi/'+videoid+'/0.jpg">'+title+'</a></li>')
-	ind += 1
+def openfile():
+    print 'Opening HTML file ...'
+    #Opening playlist.html on BBEdit
+    time.sleep(3)
+    cmd = 'bbedit playlist.html'
+    procc = subprocess.Popen(cmd , shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    procc.wait()
 
 
-file.write('</ul></div>')#Close dive
-print 'Opening HTML file ...'
-#Opening playlist.html on BBEdit
-time.sleep(3)
-cmd = 'bbedit playlist.html'
-procc = subprocess.Popen(cmd , shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-procc.wait()
+def main():
+
+    print "Playlist id?",
+    playlistid = raw_input()
+
+    generatefile(playlistid)
+    openfile()
+if __name__ == "__main__":
+    main()
